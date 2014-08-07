c31152cded06e7681ba606df70800d99f093c99c
balu92 <balugyanszky.gabor@gmail.com>
2014-08-07 09:25:59 +0200

    Revert "ILmerge all dll"

--- Example/merge.bat
+++
@@ -1,2 +0,0 @@
-ILMerge.exe /targetplatform:v2,..\..\Mono\lib\mono\2.0\ /target:library /out:IronPythonModule.dll modules\IronPythonModule\IronPythonModule.dll rust_server_data\Managed\IronPython.Deps.dll
-
