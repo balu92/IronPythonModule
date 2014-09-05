ee750b05126551e3b759fa6a88d31c77237277b5
balu92 <balugyanszky.gabor@gmail.com>
2014-09-05 14:02:04 +0200

    Moar style fix

--- IronPythonModule/IPPlugin.cs
+++ IronPythonModule/IPPlugin.cs
@@ -173,12 +173,12 @@ namespace IronPythonModule
 		public IniParser GetIni(string path) {
 			path = ValidateRelativePath(path + ".ini");
 			if (path == null)
-				return (IniParser) null;
+				return (IniParser)null;
 
 			if (File.Exists(path))
 				return new IniParser(path);
 
-			return (IniParser) null;
+			return (IniParser)null;
 		}
 
 		public bool IniExists(string path) {
@@ -193,14 +193,14 @@ namespace IronPythonModule
 			try {
 				path = ValidateRelativePath(path + ".ini");
 				if (path == null)
-					return (IniParser) null;
+					return (IniParser)null;
 
 				File.WriteAllText(path, "");
 				return new IniParser(path);
 			} catch (Exception ex) {
 				Logger.LogException(ex);
 			}
-			return (IniParser) null;
+			return (IniParser)null;
 		}
 
 		public List<IniParser> GetInis(string path) {
@@ -249,122 +249,122 @@ namespace IronPythonModule
 			Invoke ("On_TablesLoaded", tables);
 		}
 
-		public void OnAllPluginsLoaded(){
-			Invoke ("On_AllPluginsLoaded", new object[0]);
+		public void OnAllPluginsLoaded() {
+			Invoke("On_AllPluginsLoaded", new object[0]);
 		}
 
-		public void OnBlueprintUse (Fougerite.Player player, BPUseEvent evt) {
-			this.Invoke ("On_BlueprintUse", new object[] { player, evt });
+		public void OnBlueprintUse(Fougerite.Player player, BPUseEvent evt) {
+			this.Invoke("On_BlueprintUse", new object[] { player, evt });
 		}
 
-		public void OnChat (Fougerite.Player player, ref ChatString text) {
-			this.Invoke ("On_Chat", new object[] { player, text });
+		public void OnChat(Fougerite.Player player, ref ChatString text) {
+			this.Invoke("On_Chat", new object[] { player, text });
 		}
 
-		public void OnCommand (Fougerite.Player player, string command, string[] args) {
-			this.Invoke ("On_Command", new object[] { player, command, args });
+		public void OnCommand(Fougerite.Player player, string command, string[] args) {
+			this.Invoke("On_Command", new object[] { player, command, args });
 		}
 
-		public void OnConsole (ref ConsoleSystem.Arg arg, bool external) {
-			Fougerite.Player player = Fougerite.Player.FindByPlayerClient (arg.argUser.playerClient);
+		public void OnConsole(ref ConsoleSystem.Arg arg, bool external) {
+			Fougerite.Player player = Fougerite.Player.FindByPlayerClient(arg.argUser.playerClient);
 			if (!external) {
-				this.Invoke ("On_Console", new object[] { player, arg });
+				this.Invoke("On_Console", new object[] { player, arg });
 			} else {
-				this.Invoke ("On_Console", new object[] { null, arg });
+				this.Invoke("On_Console", new object[] { null, arg });
 			}
 		}
 
-		public void OnDoorUse (Fougerite.Player player, DoorEvent evt) {
-			this.Invoke ("On_DoorUse", new object[] { player, evt });
+		public void OnDoorUse(Fougerite.Player player, DoorEvent evt) {
+			this.Invoke("On_DoorUse", new object[] { player, evt });
 		}
 
-		public void OnEntityDecay (DecayEvent evt) {
+		public void OnEntityDecay(DecayEvent evt) {
 			// CONSIDER: if(!evt.Entity...IsAlive) return;
 
-			this.Invoke ("On_EntityDecay", new object[] { evt });
+			this.Invoke("On_EntityDecay", new object[] { evt });
 		}
 
-		public void OnEntityDeployed (Fougerite.Player player, Entity entity) {
-			this.Invoke ("On_EntityDeployed", new object[] { player, entity });
+		public void OnEntityDeployed(Fougerite.Player player, Entity entity) {
+			this.Invoke("On_EntityDeployed", new object[] { player, entity });
 		}
 
 		public void OnEntityDestroyed (Events.DestroyEvent evt) {
-			this.Invoke ("On_EntityDestroyed", new object[] { evt });
+			this.Invoke("On_EntityDestroyed", new object[] { evt });
 		}
 
-		public void OnEntityHurt (HurtEvent evt) {
+		public void OnEntityHurt(HurtEvent evt) {
 			if (evt.DamageEvent.status != LifeStatus.IsAlive) {
 				DamageEvent dmgEvt = evt.DamageEvent;
 				Events.DestroyEvent de = new Events.DestroyEvent(ref dmgEvt, evt.Entity, evt.IsDecay);
-				IPModule.EntityDestroyed (de);
+				IPModule.EntityDestroyed(de);
 				return;
 			}
 
 			if (evt.IsDecay)
 				return; // NOTE: this should be done in Fougerite.Hooks imo
 
-			this.Invoke ("On_EntityHurt", new object[] { evt });
+			this.Invoke("On_EntityHurt", new object[] { evt });
 		}
 
-		public void OnItemsLoaded (ItemsBlocks items) {
-			this.Invoke ("On_ItemsLoaded", new object[] { items });
+		public void OnItemsLoaded(ItemsBlocks items) {
+			this.Invoke("On_ItemsLoaded", new object[] { items });
 		}
 
-		public void OnNPCHurt (HurtEvent evt) {
-			this.Invoke ("On_NPCHurt", new object[] { evt });
+		public void OnNPCHurt(HurtEvent evt) {
+			this.Invoke("On_NPCHurt", new object[] { evt });
 		}
 
-		public void OnNPCKilled (DeathEvent evt) {
-			this.Invoke ("On_NPCKilled", new object[] { evt });
+		public void OnNPCKilled(DeathEvent evt) {
+			this.Invoke("On_NPCKilled", new object[] { evt });
 		}
 
-		public void OnPlayerConnected (Fougerite.Player player) {
-			this.Invoke ("On_PlayerConnected", new object []{ player });
+		public void OnPlayerConnected(Fougerite.Player player) {
+			this.Invoke("On_PlayerConnected", new object []{ player });
 		}
 
-		public void OnPlayerDisconnected (Fougerite.Player player) {
-			this.Invoke ("On_PlayerDisconnected", new object[] { player });
+		public void OnPlayerDisconnected(Fougerite.Player player) {
+			this.Invoke("On_PlayerDisconnected", new object[] { player });
 		}
 
-		public void OnPlayerGathering (Fougerite.Player player, GatherEvent evt) {
-			this.Invoke ("On_PlayerGathering", new object[] { player, evt });
+		public void OnPlayerGathering(Fougerite.Player player, GatherEvent evt) {
+			this.Invoke("On_PlayerGathering", new object[] { player, evt });
 		}
 
-		public void OnPlayerHurt (HurtEvent evt) {
-			this.Invoke ("On_PlayerHurt", new object[] { evt });
+		public void OnPlayerHurt(HurtEvent evt) {
+			this.Invoke("On_PlayerHurt", new object[] { evt });
 		}
 
-		public void OnPlayerKilled (DeathEvent evt) {
-			this.Invoke ("On_PlayerKilled", new object[] { evt });
+		public void OnPlayerKilled(DeathEvent evt) {
+			this.Invoke("On_PlayerKilled", new object[] { evt });
 		}
 
-		public void OnPlayerSpawn (Fougerite.Player player, SpawnEvent evt) {
-			this.Invoke ("On_PlayerSpawning", new object[] { player, evt });
+		public void OnPlayerSpawn(Fougerite.Player player, SpawnEvent evt) {
+			this.Invoke("On_PlayerSpawning", new object[] { player, evt });
 		}
 
-		public void OnPlayerSpawned (Fougerite.Player player, SpawnEvent evt) {
-			this.Invoke ("On_PlayerSpawned", new object[] { player, evt });
+		public void OnPlayerSpawned(Fougerite.Player player, SpawnEvent evt) {
+			this.Invoke("On_PlayerSpawned", new object[] { player, evt });
 		}
 
-		public void OnServerInit () {
-			this.Invoke ("On_ServerInit", new object[0]);
+		public void OnServerInit() {
+			this.Invoke("On_ServerInit", new object[0]);
 		}
 
-		public void OnServerShutdown () {
-			this.Invoke ("On_ServerShutdown", new object[0]);
+		public void OnServerShutdown() {
+			this.Invoke("On_ServerShutdown", new object[0]);
 		}
 
 		// timer hooks
 
-		public void OnTimerCB (string name) {
-			if (Globals.Contains (name + "Callback")) {
-				Invoke (name + "Callback", new object[0]);
+		public void OnTimerCB(string name) {
+			if (Globals.Contains(name + "Callback")) {
+				Invoke(name + "Callback", new object[0]);
 			}
 		}
 
-		public void OnTimerCBArgs (string name, Dictionary<string, object> args) {
-			if (Globals.Contains (name + "Callback")) {
-				Invoke (name + "Callback", args);
+		public void OnTimerCBArgs(string name, Dictionary<string, object> args) {
+			if (Globals.Contains(name + "Callback")) {
+				Invoke(name + "Callback", args);
 			}
 		}
 
@@ -372,51 +372,51 @@ namespace IronPythonModule
 
 		#region timer methods
 
-		public IPTimedEvent CreateTimer (string name, int timeoutDelay) {
-			IPTimedEvent timedEvent = GetTimer (name);
+		public IPTimedEvent CreateTimer(string name, int timeoutDelay) {
+			IPTimedEvent timedEvent = GetTimer(name);
 			if (timedEvent == null) {
-				timedEvent = new IPTimedEvent (name, (double)timeoutDelay);
-				timedEvent.OnFire += new IPTimedEvent.TimedEventFireDelegate (OnTimerCB);
+				timedEvent = new IPTimedEvent(name, (double)timeoutDelay);
+				timedEvent.OnFire += new IPTimedEvent.TimedEventFireDelegate(OnTimerCB);
 				Timers.Add(name, timedEvent);
 			}
 			return timedEvent;
 		}
 
-		public IPTimedEvent CreateTimer (string name, int timeoutDelay, Dictionary<string, object> args) {
-			IPTimedEvent timedEvent = GetTimer (name);
+		public IPTimedEvent CreateTimer(string name, int timeoutDelay, Dictionary<string, object> args) {
+			IPTimedEvent timedEvent = GetTimer(name);
 			if (timedEvent == null) {
-				timedEvent = new IPTimedEvent (name, (double)timeoutDelay);
+				timedEvent = new IPTimedEvent(name, (double)timeoutDelay);
 				timedEvent.Args = args;
-				timedEvent.OnFireArgs += new IPTimedEvent.TimedEventFireArgsDelegate (OnTimerCBArgs);
+				timedEvent.OnFireArgs += new IPTimedEvent.TimedEventFireArgsDelegate(OnTimerCBArgs);
 				Timers.Add(name, timedEvent);
 			}
 			return timedEvent;
 		}
 
-		public IPTimedEvent GetTimer (string name) {
+		public IPTimedEvent GetTimer(string name) {
 			IPTimedEvent result;
-			if (Timers.ContainsKey (name)) {
-				result = Timers [name];
+			if (Timers.ContainsKey(name)) {
+				result = Timers[name];
 			} else {
 				result = null;
 			}
 			return result;
 		}
 
-		public void KillTimer (string name) {
-			IPTimedEvent timer = GetTimer (name);
+		public void KillTimer(string name) {
+			IPTimedEvent timer = GetTimer(name);
 			if (timer != null)
 				return;
 
-			timer.Stop ();
-			Timers.Remove (name);
+			timer.Stop();
+			Timers.Remove(name);
 		}
 
-		public void KillTimers () {
+		public void KillTimers() {
 			foreach (IPTimedEvent current in Timers.Values) {
-				current.Stop ();
+				current.Stop();
 			}
-			Timers.Clear ();
+			Timers.Clear();
 		}
 
 		#endregion
