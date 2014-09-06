6a4d0c5cb7fb7bb309b897dea5daa64d95ac7d3c
balu92 <balugyanszky.gabor@gmail.com>
2014-09-06 16:34:38 +0200

    Example update

--- Example/Save/PyPlugins/Test/Test.py
+++ Example/Save/PyPlugins/Test/Test.py
@@ -9,7 +9,7 @@ from UnityEngine import Debug
 class Test:
 	def On_TablesLoaded(self, tables):
 #		Debug.Log("On_TablesLoaded hooked with " + tables.Count.ToString() + " element, from Python")
-		Plugin.TryDumpObjToFile("On_TablesLoaded.Tables", tables)
+		Plugin.DumpObjToFile("On_TablesLoaded.Tables", tables, 3, 20, True, True)
 		return tables
 
 	def On_AllPluginsLoaded(self):
@@ -27,7 +27,7 @@ class Test:
 		plug2.Invoke("TestSharedFunction", dic["first"], dic["second"])
 		Debug.Log(dic["first"])
 		Debug.Log(dic["second"])
-		Plugin.TryDumpObjToFile("testtimerCallback.dic", dic)
+		Plugin.DumpObjToFile("testtimerCallback.dict", dic, 3, 20, True, True)
 		Plugin.KillTimers()
 
 	def On_PluginInit(self):
@@ -38,76 +38,77 @@ class Test:
 
 	def On_ItemsLoaded(self, items):
 #		Debug.Log("On_ItemsLoaded hooked with " + items.Count + " element, from Python")
-		Plugin.TryDumpObjToFile("On_ItemsLoaded.Items", items)
+		Plugin.DumpObjToFile("On_ItemsLoaded.Items", items, 3, 20, True, True)
 		return items
 
 	def On_Chat(self, Player, Text):
 #		Debug.Log(Player.Name + " says: " + Text)
-		Plugin.TryDumpObjToFile("On_Chat.Player", Player)
-		Plugin.TryDumpObjToFile("On_Chat.Text", Text)
+		Plugin.DumpObjToFile("On_Chat.Player", Player, 3, 20, True, True)
+		Plugin.DumpObjToFile("On_Chat.Text", Text, 3, 20, True, True)
 
 	def On_BlueprintUse(self, Player, BPUseEvent):
 #		Debug.Log(Player.Name + " researched " + BPUseEvent.ItemName)
-		Plugin.TryDumpObjToFile("On_BlueprintUse.Player", Player)
-		Plugin.TryDumpObjToFile("On_BlueprintUse.BPUseEvent", BPUseEvent)
+		Plugin.DumpObjToFile("On_BlueprintUse.Player", Player, 3, 20, True, True)
+		Plugin.DumpObjToFile("On_BlueprintUse.BPUseEvent", BPUseEvent, 3, 20, True, True)
 
 	def On_Command(self, Player, cmd, args):
 #		Debug.Log("On_Command(" + Player.Name + ", " + cmd + ", " + args + ")")
-		Plugin.TryDumpObjToFile("On_Command.Player", Player)
-		Plugin.TryDumpObjToFile("On_Command.cmd", cmd)
-		Plugin.TryDumpObjToFile("On_Command.args", args)
+		Plugin.DumpObjToFile("On_Command.Player", Player, 3, 20, True, True)
+		Plugin.DumpObjToFile("On_Command.cmd", cmd, 3, 20, True, True)
+		Plugin.DumpObjToFile("On_Command.args", args, 3, 20, True, True)
 
 	def On_Console(self, Player, Arg):
 #		Debug.Log(Player.Name + " used " + Arg.Class + "." + Arg.Function + " in console ")
-		Plugin.TryDumpObjToFile("On_Console.Player", Player)
-		Plugin.TryDumpObjToFile("On_Console.Arg", Arg)
+		Plugin.DumpObjToFile("On_Console.Player", Player, 3, 20, True, True)
+		Plugin.DumpObjToFile("On_Console.Arg", Arg, 3, 20, True, True)
 
 	def On_DoorUse(self, Player, DoorEvent):
 #		Debug.Log(Player.Name + " tried to use a door")
 #		Debug.Log("Succeded? " + ("yes" if DoorEvent.Open else "no"))
-		Plugin.TryDumpObjToFile("On_DoorUse.Player", Player)
-		Plugin.TryDumpObjToFile("On_DoorUse.DoorEvent", DoorEvent)
+		Plugin.DumpObjToFile("On_DoorUse.Player", Player, 3, 20, True, True)
+		Plugin.DumpObjToFile("On_DoorUse.DoorEvent", DoorEvent, 3, 20, True, True)
 
 	def On_EntityDecay(self, DecayEvent):
-		Plugin.TryDumpObjToFile("On_EntityDecay.DecayEvent", DecayEvent)
+		Plugin.DumpObjToFile("On_EntityDecay.DecayEvent", DecayEvent, 3, 20, True, True)
 		return DecayEvent.DamageAmount
 
-	def On_EntityDeployed(self, Player, Enity):
-		Plugin.TryDumpObjToFile("On_EntityDeployed.Player", Player)
-		Plugin.TryDumpObjToFile("On_EntityDeployed.Enity", Enity)
+	def On_EntityDeployed(self, Player, Entity):
+		Plugin.DumpObjToFile("On_EntityDeployed.Player", Player, 3, 20, True, True)
+		Plugin.DumpObjToFile("On_EntityDeployed.Entity", Entity, 3, 20, True, True)
 
-	def On_EntityHurt(self, he):
-		Plugin.TryDumpObjToFile("On_EntityHurt.HurtEvent", he)
+	def On_EntityHurt(self, HurtEvent):
+		Plugin.DumpObjToFile("On_EntityHurt.HurtEvent", HurtEvent, 3, 20, True, True)
 
-	def On_EntityDestroyed(self, de):
-		Plugin.TryDumpObjToFile("On_EntityDestroyed.DestroyEvent", de)
+	def On_EntityDestroyed(self, DestroyEvent):
+		Plugin.DumpObjToFile("On_EntityDestroyed.DestroyEvent", DestroyEvent, 3, 20, True, True)
 
-	def On_NPCKilled(self, de):
-		Plugin.TryDumpObjToFile("On_NPCKilled.DeathEvent", de)
+	def On_NPCKilled(self, DeathEvent):
+		Plugin.DumpObjToFile("On_NPCKilled.DeathEvent", DeathEvent, 3, 20, True, True)
 
-	def On_NPCHurt(self, he):
-		Plugin.TryDumpObjToFile("On_NPCHurt.HurtEvent", he)
+	def On_NPCHurt(self, HurtEvent):
+		Plugin.DumpObjToFile("On_NPCHurt.HurtEvent", HurtEvent, 3, 20, True, True)
 
-	def On_PlayerGathering(self, Player, ge):
-		Plugin.TryDumpObjToFile("On_PlayerGathering.Player", Player)
-		Plugin.TryDumpObjToFile("On_PlayerGathering.GatherEvent", ge)
+	def On_PlayerGathering(self, Player, GatherEvent):
+		Plugin.DumpObjToFile("On_PlayerGathering.Player", Player, 3, 20, True, True)
+		Plugin.DumpObjToFile("On_PlayerGathering.GatherEvent", GatherEvent, 3, 20, True, True)
 
-	def On_PlayerSpawning(self, Player, se):
-		Plugin.TryDumpObjToFile("On_PlayerSpawn.Player", Player)
-		Plugin.TryDumpObjToFile("On_PlayerSpawn.SpawnEvent", se)
+	def On_PlayerSpawning(self, Player, SpawnEvent):
+		Plugin.DumpObjToFile("On_PlayerSpawning.Player", Player, 3, 20, True, True)
+		Plugin.DumpObjToFile("On_PlayerSpawning.SpawnEvent", SpawnEvent, 3, 20, True, True)
+		return Util.CreateVector(SpawnEvent.X, SpawnEvent.Y, SpawnEvent.Z)
 
-	def On_PlayerSpawned(self, Player, se):
-		Plugin.TryDumpObjToFile("On_PlayerSpawned.Player", Player)
-		Plugin.TryDumpObjToFile("On_PlayerSpawned.SpawnEvent", se)
+	def On_PlayerSpawned(self, Player, SpawnEvent):
+		Plugin.DumpObjToFile("On_PlayerSpawned.Player", Player, 3, 20, True, True)
+		Plugin.DumpObjToFile("On_PlayerSpawned.SpawnEvent", SpawnEvent, 3, 20, True, True)
 
-	def On_PlayerKilled(self, de):
-		Plugin.TryDumpObjToFile("On_PlayerKilled.DeathEvent", de)
+	def On_PlayerKilled(self, DeathEvent):
+		Plugin.DumpObjToFile("On_PlayerKilled.DeathEvent", DeathEvent, 3, 20, True, True)
 
-	def On_PlayerHurt(self, he):
-		Plugin.TryDumpObjToFile("On_PlayerHurt.HurtEvent", he)
+	def On_PlayerHurt(self, HurtEvent):
+		Plugin.DumpObjToFile("On_PlayerHurt.HurtEvent", HurtEvent, 3, 20, True, True)
 
 	def On_PlayerConnected(self, Player):
-		Plugin.TryDumpObjToFile("On_PlayerConnected.Player", Player)
+		Plugin.DumpObjToFile("On_PlayerConnected.Player", Player, 3, 20, True, True)
 
-	def On_PlayerDisconnected(self, he):
-		Plugin.TryDumpObjToFile("On_PlayerDisconnected.Player", Player)
+	def On_PlayerDisconnected(self, Player):
+		Plugin.DumpObjToFile("On_PlayerDisconnected.Player", Player, 3, 20, True, True)
